0efbbad90a235e2f0fdc8a235eb63bac6e0d980a
Luke Thompson <16175583curtin@gmail.com>
2021-10-17 17:49:51 +0800

    complete page changing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import PortfolioContainer from "./Components/PortfolioContainer";
+// import NavTabs from './NavTabs';
+import Home from './Components/pages/Home';
+import About from './Components/pages/About';
+import Blog from './Components/pages/Blog';
+import Contact from './Components/pages/Contact';
+
+
+
+
+
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState('Home');
+
+// This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+const renderPage = () => {
+  if (currentPage === 'Home') {
+    return <Home />;
+  }
+  if (currentPage === 'About') {
+    return <About />;
+  }
+  if (currentPage === 'Blog') {
+    return <Blog />;
+  }
+  return <Contact />;
+};
+
+const handlePageChange = (page) => setCurrentPage(page);
+
+
   return <div>
-  <Header />
-  <PortfolioContainer />
+  <Header handlePageChange={handlePageChange} currentPage={currentPage}/>
+  <PortfolioContainer renderPage={renderPage} />
   <Footer />
 
   </div>
--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import NavTabs from "./NavTabs"
 
-function Header() {
+function Header({ currentPage, handlePageChange }) {
   return (
 <nav class="navbar navbar-expand-lg py-4 navbar-dark bg-dark d-flex justify-content-around">
   <a class="navbar-brand" href="#"><h1>Luke Thompson</h1></a>
-    <NavTabs />
+    <NavTabs handlePageChange={handlePageChange} currentPage={currentPage}/>
 </nav>
   );
 }
