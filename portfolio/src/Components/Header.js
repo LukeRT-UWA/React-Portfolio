2a3290f91625c5684aa5abb4cd2e919f0db1e631
Luke Thompson <16175583curtin@gmail.com>
2021-10-17 16:19:49 +0800

    begin page switching logic

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,26 +1,11 @@
 import React from "react";
+import NavTabs from "./NavTabs"
 
 function Header() {
   return (
 <nav class="navbar navbar-expand-lg py-4 navbar-dark bg-dark d-flex justify-content-around">
   <a class="navbar-brand" href="#"><h1>Luke Thompson</h1></a>
-
-  <div id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><h2>About me</h2></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"><h2>Portfolio</h2></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"><h2>Contact</h2></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"><h2>Resume</h2></a>
-      </li>
-    </ul>
-  </div>
+    <NavTabs />
 </nav>
   );
 }
