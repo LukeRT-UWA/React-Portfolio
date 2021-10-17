5c7e125055417aa967e5a026496bcf762072d77a
Luke Thompson <16175583curtin@gmail.com>
2021-10-17 17:08:40 +0800

    rendering pages in header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import PortfolioContainer from "./Components/PortfolioContainer";
 
 function App() {
   return <div>
   <Header />
+  <PortfolioContainer />
   <Footer />
 
   </div>
