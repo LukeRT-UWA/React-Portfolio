68ffe6f4a282954401fbb04a781c6d1cf9d76570
Luke Thompson <16175583curtin@gmail.com>
2021-10-17 12:34:31 +0800

    file structure

---
+++ portfolio/src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+   <header>
+        <div>
+            <h1>Portfolio</h1>
+        </div>
+        <nav>
+            <ul>
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+            </ul>
+        </nav>
+   </header>
+  );
+}
+
+
+export default Header;
