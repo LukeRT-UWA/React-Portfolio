3931714adf960ceec019e140096be7ad6a49ac3e
Luke Thompson <16175583curtin@gmail.com>
2021-10-17 13:37:11 +0800

    navbar complete

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -2,18 +2,26 @@ import React from "react";
 
 function Header() {
   return (
-   <header>
-        <div>
-            <h1>Portfolio</h1>
-        </div>
-        <nav>
-            <ul>
-                <li>Link 1</li>
-                <li>Link 2</li>
-                <li>Link 3</li>
-            </ul>
-        </nav>
-   </header>
+<nav class="navbar navbar-expand-lg py-4 navbar-dark bg-dark d-flex justify-content-around">
+  <a class="navbar-brand" href="#"><h1>Luke Thompson</h1></a>
+
+  <div id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><h2>About me</h2></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><h2>Portfolio</h2></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><h2>Contact</h2></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><h2>Resume</h2></a>
+      </li>
+    </ul>
+  </div>
+</nav>
   );
 }
 
