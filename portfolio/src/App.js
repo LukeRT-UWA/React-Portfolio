a8772aa1be9c7582328a7067f9307f4295483629
Luke Thompson <16175583curtin@gmail.com>
2021-10-17 14:20:47 +0800

    learned how to render multiple components, started footer

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <Header /> 
-  
+  return <div>
+  <Header />
+  <Footer />
+
+  </div>
+
+
 }
 
 export default App;
